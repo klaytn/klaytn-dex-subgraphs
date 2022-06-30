64921b9eda671b9a502199571967b84b3ff6ac8e
Sergey <travoltaj237@gmail.com>
2022-06-30 14:20:33 +0300

    [Update] Staking contract deployed

--- subgraphs/staking/src/utils/index.ts
+++ subgraphs/staking/src/utils/index.ts
@@ -3,7 +3,7 @@ import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
 export const ZERO_BI = BigInt.fromI32(0);
 export const ONE_BI = BigInt.fromI32(1);
-export const FACTORY_ADDRESS = "0x927158be21fe3d4da7e96931bb27fd5059a8cbc2";
+export const FACTORY_ADDRESS = "0xeB81FCD447957d2AE1fBE7676A4684442b9aE941";
 
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
   let bd = BigDecimal.fromString("1");
