43e3e130400e97a5424188b2b9cf93e5afccf773
German Vechtomov <d4emonset@gmail.com>
2022-11-04 12:32:30 +0300

    added shebang; rename

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cp $KLAYTN_CONTRACT_DEPLOYMENT_DIR/networks-DexFactory.json /app/subgraphs/exchange/networks.json
 cp $KLAYTN_CONTRACT_DEPLOYMENT_DIR/networks-Farming.json /app/subgraphs/farming/networks.json
 cp $KLAYTN_CONTRACT_DEPLOYMENT_DIR/networks-StakingFactory.json /app/subgraphs/staking/networks.json
