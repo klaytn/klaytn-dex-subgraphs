af0b7d67b6c89a1355159a4159fb44a034fd477f
German Vechtomov <d4emonset@gmail.com>
2022-11-07 17:41:08 +0300

    added version label env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,12 @@ if [ -z $DEX_GRAPHNODE_ADMIN_URL ] ; then
         exit 1
 fi
 
+if [ -z $DEX_VERSION_LABEL ] ; then
+        echo "Please set DEX_VERSION_LABEL."
+
+        exit 1
+fi
+
 cp $DEX_CONTRACT_DEPLOYMENT_DIR/networks-DexFactory.json /app/subgraphs/exchange/networks.json
 cp $DEX_CONTRACT_DEPLOYMENT_DIR/networks-Farming.json /app/subgraphs/farming/networks.json
 cp $DEX_CONTRACT_DEPLOYMENT_DIR/networks-StakingFactory.json /app/subgraphs/staking/networks.json
